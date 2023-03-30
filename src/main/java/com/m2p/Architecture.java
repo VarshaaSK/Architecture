66cfff3aa7e2a8b8db48d534c232d35e6c9a122c
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-30 12:00:27 +0530

    Add . Req 1 : Included the Logic and Test case for checking the equality of unit

--- src/main/java/com/m2p/Architecture.java
+++ src/main/java/com/m2p/Architecture.java
@@ -1,4 +1,10 @@
 package com.m2p;
 
 public class Architecture {
+    public String checkForValue(String unit1, String unit2) {
+        if(unit1.equals(unit2)){
+            return "Both Are Same";
+        }
+        return "Both are different";
+    }
 }
--- src/test/java/com/m2p/ArchitectureTest.java
+++ src/test/java/com/m2p/ArchitectureTest.java
@@ -1,5 +1,27 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+
+
 public class ArchitectureTest {
     Architecture architecture = new Architecture();
+
+    @Test
+    void toCheckIfBothUnitsAreSame(){
+
+        //Arrange
+        String expectedValue = "Both Are Same";
+
+        //Act
+        String actualValue = architecture.checkForValue("1 cm" , "1 cm");
+
+        //Assert
+//        assertEquals(expectedValue, actualValue);
+        assertThat(actualValue , is(equalTo(expectedValue)));
+    }
 }
